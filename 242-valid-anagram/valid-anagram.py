f61f485835f1f1ffe94e7af517de1c04bc96f70f
Kowshika Reddy <104723434+KowshiReddy@users.noreply.github.com>
2025-04-17 21:44:13 -0400

    Time: 3 ms (98.35%) | Memory: 17.8 MB (86.51%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,7 +1,15 @@
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if sorted(s) == sorted(t):
+#             return True
+#         return False
+
+## this has the time complexity of O(nlogn) --> for sorted array
+## and space is O(n)
+
+##Method 2: use counter ..this has time complexity of O(n) 
+## and space complexity is O(n)
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if sorted(s) == sorted(t):
-            return True
-        return False
-
-        
+        return Counter(s) == Counter(t)
